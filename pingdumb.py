d2627960200e7a38634672de2d2148e9cafd4f34
kyunooh <chm073@gmail.com>
2015-11-24 00:21:34 +0900

    update parameter logic

--- pingdumb.py
+++ pingdumb.py
@@ -1,20 +1,29 @@
+import getopt
+import getpass
 import sys
 
-from smtp_module import smtp_login_with_conf_test
+from conf import read_config, set_config, write_config
 from main_module import checker
-from conf import set_config, write_config, read_config
+from smtp_module import smtp_login_with_conf_test
 
 
 def main(argv):
-
+    
     if len(sys.argv) == 1:
         conf = set_config()
         s_pw = conf["smtpPw"]
         write_config(conf)
     else:
         """if exist argv, set password and execute with default configure"""
+        opts, args = getopt.getopt(argv, 'p')
+        for o, a in opts:
+            if o == "-p":
+                if a == "":
+                    s_pw = getpass.getpass("SMTP Server password", "")
+                else:
+                    s_pw = a
+
         conf = read_config()
-        s_pw = sys.argv[1]
         
     conf["smtpPw"] = s_pw
     smtp_login_with_conf_test(conf)
