5e715ab499b32c76a8c0d6a0cde21663e6e7ef62
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-02-16 19:17:10 +0900

    update version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name = 'pingdumb',
     packages = ['pingdumb'],
-    version = '0.1.3',
+    version = '0.1.4',
     description = 'Check http status on console',
     author = 'Hyeon-Mook Jerry Choi',
     author_email = 'chm073@gmail.com',
     url = 'https://github.com/kyunooh/pingdumb',
-    download_url = 'https://github.com/kyunooh/pingdumb/tarball/0.1.3',
+    download_url = 'https://github.com/kyunooh/pingdumb/tarball/0.1.4',
     keywords = ['testing', 'loggin', 'example'],
     classifiers = []
 )
